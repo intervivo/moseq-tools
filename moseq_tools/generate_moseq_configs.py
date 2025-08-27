a720630e6464691c7cb82b0de10ac087284753c0
Dan Milgram <danm@vivocore.com>
2025-08-27 14:29:49 -0400

    Fix generate_moseq_configs
    - Ensure that source YAML data files referenced by command are included in package distribution
    - Fix ruamel.yaml calls to adhere to current library API as deprecated functions have been removed in the most recent version of the package

--- moseq_tools/generate_moseq_configs.py
+++ moseq_tools/generate_moseq_configs.py
@@ -1,14 +1,20 @@
 #!/usr/bin/env python
+from importlib.resources import files
 import click
 import shutil
 import ruamel.yaml as yaml
-from pathlib import Path
+
 
 
 @click.group()
 def cli():
     pass
 
+def load_schema(filename):
+    resource = files("moseq_tools.data") / filename
+    with resource.open("r") as f:
+        data = yaml.YAML(typ='safe', pure=True)
+        return data.load(f)
 
 @cli.command("extract")
 @click.option("--flip-classifier-path", type=click.Path(exists=True), required=True, help="Path to the flip classifier YAML file.")
@@ -16,12 +22,11 @@ def cli():
 def generate_extraction_config(flip_classifier_path, output_path):
     """Generates configuration file for `moseq2-extract`."""
     print(f"Flip classifier path: {flip_classifier_path}")
-    config_path = Path(__file__).resolve().parent / "extraction-config.yaml"
-    with open(config_path, "r") as f:
-        config = yaml.safe_load(f)
+    config = load_schema("extraction-config.yaml")
     config["flip_classifier"] = flip_classifier_path
     with open(output_path, "w") as f:
-        yaml.safe_dump(config, f)
+        data = yaml.YAML(typ='safe', pure=True)
+        data.dump(config, f)
 
     print(f"Extraction configuration saved to {output_path}")
 
@@ -30,8 +35,8 @@ def generate_extraction_config(flip_classifier_path, output_path):
 @click.option("--output-path", type=click.Path(), default="pca-config.yaml", help="Path to save the generated PCA configuration.")
 def generate_model_config(output_path):
     """Generates configuration file for `moseq2-pca`."""
-    config_path = Path(__file__).resolve().parent / "pca-config.yaml"
-
+    resource = files("moseq_tools.data") / "pca-config.yaml"
+    config_path = resource.locate()
     shutil.copyfile(config_path, output_path)
 
     print(f"PCA configuration saved to {output_path}")
